93d43821864b152dd01d896264759d2ea7a941d1
michaelvuolo <michaelvuolo1@gmail.com>
2021-11-20 17:38:15 -0500

    done

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Michael Vuolo
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // init variables
+    double operand1, operand2, result;
+    char operation;
+
+    // read operation and operands from user
+    cin >> operation >> operand1 >> operand2;
+
+    // compute based on operation
+    if (operation == '+')
+        result = operand1 + operand2;
+    else if (operation == '-')
+        result = operand1 - operand2;
+    else if (operation == '*')
+        result = operand1 * operand2;
+    else if (operation == '/')
+        result = operand1 / operand2;
+
+    // display result
+    cout << "Result: " << result;
+}
